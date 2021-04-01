2e4b9dc499196d395fc7bcb55bcd4da39a92face
Niga9L <lyaginvanya@gmail.com>
2021-04-01 18:27:26 +0300

    Добавлено подключение к БД и подтверждение для vk-callback-api

--- api/src/vk-callback-api/vk-callback-api.controller.ts
+++ api/src/vk-callback-api/vk-callback-api.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post } from '@nestjs/common';
 import { VkCallbackApiService } from './vk-callback-api.service';
 
 @Controller('/callback-vk')
@@ -6,6 +6,13 @@ export class VkCallbackApiController {
   constructor(private readonly _vkCallbackService: VkCallbackApiService) {}
   @Get()
   sendOk() {
+    return 'ok';
+  }
+
+  @Post()
+  @HttpCode(200)
+  @Header('Content-Type', 'application/json')
+  requestFromVK() {
     return this._vkCallbackService.sendConfirmAnswer();
   }
 }
